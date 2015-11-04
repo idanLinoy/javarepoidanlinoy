5b98ff63531599a28e6b3fc9dbd9cf326e547d7b
Linoy Eler <linoya930@gmail.com>
2015-11-04 16:53:15 +0200

    initial

---
+++ src/com/myproj/javacourse/MtaprojlinoyServlet.java
@@ -0,0 +1,15 @@
+package com.myproj.javacourse;
+import java.io.IOException;
+import javax.servlet.http.*;
+/**
+ * 
+ * @author Linoy
+ *
+ */
+@SuppressWarnings("serial")
+public class MtaprojlinoyServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
