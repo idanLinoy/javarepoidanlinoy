5ab43a0c8e29f152d016ec1b5a5043e4cea90146
Linoy Eler <linoya930@gmail.com>
2015-11-21 21:07:08 +0200

    Fix for ex3

--- src/com/myproj/javacourse/Exercise3Servlet.java
+++ src/com/myproj/javacourse/Exercise3Servlet.java
@@ -16,7 +16,8 @@ public class Exercise3Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
 		
-		String resultStr = MathEx3.getResults();
+		MathEx3 mathEx3 = new MathEx3();
+		String resultStr = mathEx3.getResults();
 		resp.getWriter().println(resultStr);
 
 	}
