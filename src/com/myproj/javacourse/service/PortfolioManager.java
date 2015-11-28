a7ce872fc1cb2ceee7e23d3a635a418e251d6e9b
Linoy Eler <linoya930@gmail.com>
2015-11-28 15:41:17 +0200

    ex 5

---
+++ src/com/myproj/javacourse/service/PortfolioManager.java
@@ -0,0 +1,26 @@
+package com.myproj.javacourse.service;
+import java.util.Calendar;
+
+import com.myproj.javacourse.Stock;
+import com.myproj.javacourse.model.Portfolio;
+
+public class PortfolioManager {
+	
+	public Portfolio getPortfolio() {
+		Stock stock1, stock2, stock3;
+		Portfolio portfolio = new Portfolio();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2014, 10, 15);
+		
+		stock1 = new Stock ("PIH", 13.1f, 12.4f,cal.getTime());
+		stock2 = new Stock ("AAL", 5.78f, 5.5f, cal.getTime());
+		stock3 = new Stock ("CAAS", 32.2f, 31.5f, cal.getTime());
+		
+		portfolio.addStock(stock1);
+		portfolio.addStock(stock2);
+		portfolio.addStock(stock3);
+		
+		return portfolio;
+	}
+}
